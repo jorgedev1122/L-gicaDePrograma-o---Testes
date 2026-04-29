225062162e0517393378d63d5814bfa327051a80
Jorge Enrique <jorgeenriqueantunes@gmail.com>
2026-04-28 21:41:17 -0300

    Pequenas mudancinhas

--- teste.py
+++ teste.py
@@ -4,3 +4,4 @@ idade = int(input("Digite sua idade: "))
 if idade >= 18:
     print("Você é maior de idade, pode entrar na balada!")
 else:    print("Desculpe, você é menor de idade, não pode entrar na balada.")
+
